7c2b4e0e6b4fe60c6f415c59b6d238c9ce5d509f
Venkatesh Tadinada <ven@solivarlabs.com>
2019-02-12 20:03:46 -0800

    v3

--- controller.py
+++ controller.py
@@ -12,7 +12,8 @@ def home():
         bed =   request.form.get('bed')
         bath =   request.form.get('bath')
         floors = request.form.get('floors')
-        price = 400*int(sqrFeet)+100*int(size)+40000*int(bed)+100000*int(floors)+50000*int(bath)
+        l = [sqrFeet,size,bed,bath,floors]
+        price = model.predict([l])[0]
         return render_template("home.html",price=price,sqrFeet=sqrFeet,size=size,bed=bed,bath=bath,floors=floors)
     sqrFeet= 0
     size=  0
@@ -22,8 +23,11 @@ def home():
     return render_template("home.html",sqrFeet=sqrFeet,size=size,bed=bed,bath=bath,floors=floors)
 
 
-
+import pickle
+import sklearn
 
 
 if __name__ == '__main__':
+    infile = open('model.pickle','rb')
+    model = pickle.load(infile,encoding='latin1')
     app.run(debug=True)
